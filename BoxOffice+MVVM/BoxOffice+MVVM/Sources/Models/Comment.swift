b5df012040ebc5a51f9762a103f1364df19ced2d
haeseoklee <haeseoklee.dev@gmail.com>
2022-01-06 01:10:28 +0900

    [#9] Comment에 empty 속성 추가

--- BoxOffice+MVVM/BoxOffice+MVVM/Sources/Models/Comment.swift
+++ BoxOffice+MVVM/BoxOffice+MVVM/Sources/Models/Comment.swift
@@ -14,6 +14,7 @@ struct Comment: Codable {
     let writer: String
     let movieId: String
     let contents: String
+    static let empty: Comment = Comment(id: nil, rating: 0, timestamp: 0, writer: "", movieId: "", contents: "")
     
     enum CodingKeys: String, CodingKey {
         case id, rating, timestamp, writer, contents
